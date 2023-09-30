c6c7a30429216a1577f26b5a6e06f7713e90d1f9
TechnoTeam-17 <142696425+TechnoTeam-17@users.noreply.github.com>
2023-09-30 20:03:07 +0200

    Add files via upload

---
+++ src/test/java/Runners/_06_HesaplarimArasiParaTransferiRunner.java
@@ -0,0 +1,14 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+        tags = "@SmokeTest ,@Regression",
+        features = {"src/test/java/featureFiles/_06_Hesaplarim_Arasi_Para_Transferi.feature"},
+        glue={"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class _06_HesaplarimArasiParaTransferiRunner extends AbstractTestNGCucumberTests{
+}
