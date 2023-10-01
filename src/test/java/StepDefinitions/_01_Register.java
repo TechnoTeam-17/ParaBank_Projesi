f4199fe5bf2b52a8d5eb75c7ad9a1cdfd14a8dfd
nesterch <terlemez.neslihan@gmail.com>
2023-10-01 22:20:10 +0200

    US_01 bitti

--- src/test/java/StepDefinitions/_01_Register.java
+++ src/test/java/StepDefinitions/_01_Register.java
@@ -17,13 +17,10 @@ public class _01_Register  {
     public void navigateToPrestaShop() {
         GWD.getDriver().get("https://para.testar.org/");
     }
-
     @When("Enter click Register in button")
     public void clickRegisterinbutton() {
     dc.myClick(dc.Registerbutton);
-        //String emailfake=fk.name().username().concat("@gmail.com");
-        //dc.mySendKeys(dc.userName,emailfake);
-        //dc.myClick(dc.CreateAccount);
+
     }
 
     @Then("Enter Data in Text Box")
@@ -36,17 +33,15 @@ public class _01_Register  {
         dc.mySendKeys(dc.zipcode,"13599");
         dc.mySendKeys(dc.phone, "0792222222");
         dc.mySendKeys(dc.ssn,"2023");
-        dc.mySendKeys(dc.userName,"miriam");
+        String emailfake=fk.name().username();
+        dc.mySendKeys(dc.userName,emailfake);
+
+      //  dc.mySendKeys(dc.userName,"miriam");
         dc.mySendKeys(dc.password,"miriam2023");
         dc.mySendKeys(dc.confirm,"miriam2023");
-                         }
-    @And("Enter click Register")
-    public void enterClickRegister() {
-        String emailfake=fk.name().username().concat("@gmail.com");
-        dc.mySendKeys(dc.userName,emailfake);
-        dc.myClick(dc.CreateAccount);
         dc.myClick(dc.REGISTER);
-    }
+                         }
+
    @And("User should login successfully")
     public void userShouldLoginSuccessfully()throws InterruptedException {
         dc.verifyContainsText(dc.verifyLogin,"Welcome");
