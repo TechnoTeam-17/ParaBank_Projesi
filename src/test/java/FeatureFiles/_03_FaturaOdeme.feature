36f584fe862a52876148d48d480b09eb3d40fbbc
AyGulll <abilovaygul@gmail@com>
2023-10-06 11:48:56 +0100

    _03_Kullanici girisi hata veriyor

--- src/test/java/FeatureFiles/_03_FaturaOdeme.feature
+++ src/test/java/FeatureFiles/_03_FaturaOdeme.feature
@@ -1,15 +1,16 @@
 Feature: Paying Bills
 
 
-  Background:
+   Background:
 
     Given Navigate To ParaBank
     When Enter username and password and click login button
     Then User should log-in successfully
 
   @SmokeTest
-  Scenario: Account Overview
+  Scenario : Account Overview
     When Click Bill Payment Service
     Then Enter payee information
     And Send Payment
+
 
--- src/test/java/StepDefinitions/_03_FaturaOdeme.java
+++ src/test/java/StepDefinitions/_03_FaturaOdeme.java
@@ -10,51 +10,15 @@ import java.awt.*;
 
 public class _03_FaturaOdeme {
 
-    DialogContent dc=new DialogContent();
-
- //  @When("Enter username and password and click login button")
- //  public void login() {
- //
- //
- //
- //
- //
- //
- //
- //
- //
- //
- //
- //
-
- //  }
-    @Then("Bill Payment was successful")
-    public void billpayment(){dc.verifyContainsText(dc.verifyLogin,"Bill Payment Complete");}
-
- //  @Given("Navigate to parabank")
- //  public void navigateToParabank() {
-
- //  }
+    DialogContent dc = new DialogContent();
 
     @When("Click Bill Payment Service")
     public void clickBillPaymentService() {
         dc.myClick(dc.BillPay);
-
-
-
-
-
-
-
-
-
-
-
     }
 
     @Then("Enter payee information")
     public void enterPayeeInformation() {
-
         dc.mySendKeys(dc.PayeeName, "Mina");
         dc.mySendKeys(dc.Address, "America");
         dc.mySendKeys(dc.City, "Los-Angeles");
@@ -64,14 +28,11 @@ public class _03_FaturaOdeme {
         dc.mySendKeys(dc.Account, "12345");
         dc.mySendKeys(dc.VerifyAccount, "12345");
         dc.mySendKeys(dc.Amount, "100");
-        dc.selectEngineByID("fromAccountId",0);
-
+        dc.selectEngineByID("fromAccountId", 0);
     }
 
     @And("Send Payment")
     public void sendPayment() {
-
-
         dc.myClick(dc.SendPayment);
     }
 }
