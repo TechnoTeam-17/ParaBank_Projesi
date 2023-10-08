9b77fb32a3cd12690521461d774822ff34d0a689
TechnoTeam-17 <142696425+TechnoTeam-17@users.noreply.github.com>
2023-10-08 10:41:04 +0200

    Update _06_HesaplarimArasiParaTransferiRunner.java

--- src/test/java/Runners/_06_HesaplarimArasiParaTransferiRunner.java
+++ src/test/java/Runners/_06_HesaplarimArasiParaTransferiRunner.java
@@ -5,10 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        tags = "@SmokeTest ,@Regression",
+        tags = "@SmokeTest",
         features = {"src/test/java/featureFiles/_06_Hesaplarim_Arasi_Para_Transferi.feature"},
         glue={"StepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class _06_HesaplarimArasiParaTransferiRunner extends AbstractTestNGCucumberTests{
 }
+
